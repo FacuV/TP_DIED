89326780ebb0562aba5e4bdda6eb8121ae0b5312
unknown <Facundo Velazco>
2020-07-20 14:02:21 -0300

    ClasesCreadasV3

--- src/Negocio/Insumo.java
+++ src/Negocio/Insumo.java
@@ -1,4 +1,12 @@
 package Negocio;
 
-public class Insumo {
+public abstract class Insumo {
+    private int id_insumo;
+    private String descripcion;
+    private String unidad_medida;
+    private double costo;
+
+
+
+    public abstract double pesoPorUnidad();
 }
