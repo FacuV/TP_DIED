1dec4369d8f656a6a813cd1b71e8b2e1e813d8e5
Facundo Velazco <facundovelazco13@gmail.com>
2020-07-30 15:47:47 -0300

    insumodao

--- src/main/java/Daos/InsumoDaoDB.java
+++ src/main/java/Daos/InsumoDaoDB.java
@@ -1,6 +1,8 @@
 package Daos;
 
 import Negocio.Insumo;
+import Negocio.Insumo_General;
+import Negocio.Insumo_Liquido;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -13,7 +15,16 @@ public class InsumoDaoDB implements InsumoDao{
         Statement stmt = conexion.createStatement();
         String id_insumo = String.valueOf(insumo.getId_insumo());
         String costo = String.valueOf(insumo.getCosto());
-        stmt.execute("INSERT INTO insumo VALUES ('"+id_insumo+"','"+insumo.getDescripcion()+"','"+insumo.getUnidad_medida() );
+        stmt.execute("INSERT INTO insumo VALUES ('"+id_insumo+"','"+insumo.getDescripcion()+"','"+insumo.getUnidad_medida()+"','"+costo+"')");
+        if(insumo instanceof Insumo_General){
+            String peso = String.valueOf(((Insumo_General) insumo).getPeso_kilos());
+            stmt.execute("INSERT INTO general VALUES ('"+id_insumo+"','"+peso+"')");
+        }else{
+            String densidad = String.valueOf(((Insumo_Liquido) insumo).getDensidad());
+            stmt.execute("INSERT INTO liquido VALUES ('"+id_insumo+"','"+densidad+"')");
+        }
+        stmt.close();
+        conexion.close();
     }
 
     @Override
