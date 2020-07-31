6ef31abfb1f503abb205b65262247021a80c50d0
Eira Micaela Martinez <eiramicaelamartinez@gmail.com>
2020-07-30 21:12:11 -0300

    Hice StockDao

--- src/main/java/Daos/StockDao.java
+++ src/main/java/Daos/StockDao.java
@@ -3,12 +3,13 @@ package Daos;
 import Negocio.Stock;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public interface StockDao {
 
     public void createStock(Stock stock) throws SQLException;
     public void deleteStock(Stock stock) throws SQLException;
     public void updateStock(Stock stock) throws SQLException;
-    public Stock getStock(int id_stock) throws SQLException;
+    public ArrayList getStock(int id_planta) throws SQLException;
 
 }
