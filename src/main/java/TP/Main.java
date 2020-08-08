a56b415f12aecae97cdf2bb274ea16346c065cfc
unknown <Facundo Velazco>
2020-08-08 09:54:20 -0300

    interfaz

--- src/main/java/TP/Main.java
+++ src/main/java/TP/Main.java
@@ -17,10 +17,12 @@ public class Main {
 
     public static void main(String[] args) throws SQLException {
         PlantaDaoDB plantaDao = new PlantaDaoDB();
+
         Gestor_Plantas.setPlantas(plantaDao.getPlanta());
+        //Gestor_Plantas.setRutas();
 
         Gestor_Pantalla.visualizarPantalla_principal();
-
+        //System.out.println(Gestor_Plantas.flujoMaxNumero(Gestor_Plantas.getPlanta(1),Gestor_Plantas.getPlanta(10)));
         /*
         //para probar funcion rutaPosibles() y plantasConStock();
         Gestor_Plantas.registrarPlanta("planta1");
