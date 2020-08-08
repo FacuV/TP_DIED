d4d696a51a6aa46019cfcdd7928c0b3d6eb4ce1f
Eira Micaela Martinez <eiramicaelamartinez@gmail.com>
2020-08-08 17:20:01 -0300

    para pushear

--- src/main/java/Daos/Orden_PedidoDao.java
+++ src/main/java/Daos/Orden_PedidoDao.java
@@ -8,5 +8,4 @@ public interface Orden_PedidoDao {
     public void createOrden_Pedido(Orden_Pedido orden_pedido) throws SQLException;
     public void deleteOrden_Pedido(Orden_Pedido orden_pedido) throws SQLException;
     public void updateOrden_Pedido(Orden_Pedido orden_pedido) throws SQLException;
-    public Orden_Pedido getOrden_Pedido(int numero_orden) throws SQLException;
 }
